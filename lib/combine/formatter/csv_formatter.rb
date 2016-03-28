4183195359a94816f31a0861d26f8c4abab600da
Abeer Salameh <abeer.salameh@gmail.com>
2016-03-28 07:33:42 +0000

    Add seperate module to format json files.

---
+++ lib/combine/formatter/csv_formatter.rb
@@ -0,0 +1,15 @@
+module Formatter
+  module CsvFormatter
+    ACCEPTED_FORMATS = [".csv"]
+
+    def self.read_from(input)
+      validate_format(input)
+      CSV.read(input).reject(&:empty?)
+    end
+
+    def self.validate_format(file)
+      raise ::Combine::InvalidFileFormatError unless
+        ACCEPTED_FORMATS.include? File.extname(file)
+    end
+  end
+end
